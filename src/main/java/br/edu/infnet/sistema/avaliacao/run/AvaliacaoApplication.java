f5c0b18740b2502cdafdb83de3e7bb6ca87e3930
Leonam <Leonam>
2018-08-11 12:06:44 -0300

    Fix create table issue

--- src/main/java/br/edu/infnet/sistema/avaliacao/run/AvaliacaoApplication.java
+++ src/main/java/br/edu/infnet/sistema/avaliacao/run/AvaliacaoApplication.java
@@ -2,10 +2,12 @@ package br.edu.infnet.sistema.avaliacao.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"br.edu.infnet.sistema.avaliacao.model"})
 public class AvaliacaoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
