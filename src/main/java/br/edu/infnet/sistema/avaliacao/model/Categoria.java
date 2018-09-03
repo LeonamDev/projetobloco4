d9e61483fc926d78fb4844292e776635090cf26e
andre.luizsantos <andre.luizsantos@RJOD269650.GOV>
2018-09-02 22:59:11 -0300

    Incluindo a classe Categoria das Questões

---
+++ src/main/java/br/edu/infnet/sistema/avaliacao/model/Categoria.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.sistema.avaliacao.model;
+
+public enum Categoria {
+
+    AVALIACAO_GERAL_POS_GRADUACAO,
+    AVALIACAO_PROFESSOR_MODULO,
+    AVALIACAO_CONTEUDO_INFRA_ESTRUTUTA_MODULO;
+    
+}
